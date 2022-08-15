a1e4395f0d9584a9e8f8dda746634abfe9b0505e
Gabriella_Akpo <ella9894@gmail.com>
2022-08-15 19:39:25 -0400

    added the localStorage code

--- assets/js/Scheduler.js
+++ assets/js/Scheduler.js
@@ -1,6 +1,9 @@
 var date = moment().format('dddd, MMMM Do');
 var currentDay = document.querySelector('#currentDay');
 var time = moment().format('hA');
+console.log(time);
+var taskArray = JSON.parse(localStorage.getItem(('tasks'))) || [];
+console.log(taskArray);
 
 $("#currentDay").html(date);
 
@@ -17,17 +20,27 @@ $(".time-block").each(function () {
         $(this).removeClass("past present");
         $(this).addClass("future");
     }
+
+    taskArray.forEach(element => {
+        if (element.savedTime === blockHour) {
+            $(this).children(".description").val(element.text);
+        }
+        
+    });
 });
 
 $(".saveBtn").click(function () {
-    var text = $(".description");
-    // var savedTime = $("this").parent("text");
+    var text = $(this).siblings(".description").val();
+   
+    var savedTime = $(this).siblings(".hour").text().trim();
+    var TimeText = { text, savedTime };
+    taskArray.push(TimeText);
         
-    localStorage.setItem("task",JSON.stringify(text));
+    localStorage.setItem("tasks",JSON.stringify(taskArray));
 })
 
-$(".description").each(function () {
-    localStorage.getItem('test');
-})
+
+
+
 
     
