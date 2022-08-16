86a7e48865752443634451cbf94782ed060b589b
Gabriella_Akpo <ella9894@gmail.com>
2022-08-16 12:49:19 -0400

    fixed the timeblock coloring

--- assets/js/Scheduler.js
+++ assets/js/Scheduler.js
@@ -1,47 +1,34 @@
 var date = moment().format('dddd, MMMM Do');
 var time = moment().hour();
-console.log(typeof time, time);
-var taskArray = JSON.parse(localStorage.getItem(('tasks'))) || [];
-
-
+var taskArray = JSON.parse(localStorage.getItem(("tasks"))) || [];
 
 $("#currentDay").html(date);
 
 $(".time-block").each(function () {
-    // var blockHour = parseInt($(this).children(".hour").text().trim());
-    var block = parseInt($(this).children("id").text().trim());
-    console.log(block);
-    // console.log(typeof blockHour, blockHour);
-        
-    if (block < time) {
-        // $(this).removeClass("present")
-        // $(this).removeClass("future")
+    var blockHour = parseInt($(this).attr("id").split('-')[1]);
+    if (blockHour < time) {
         $(this).addClass("past");
-    } else if (block === time) {
-        // $(this).removeClass("past future");
+    } else if (blockHour === time) {
         $(this).addClass("present");
     } else {
-        // $(this).removeClass("past present");
-        $(this).addClass("future");
+        $(this).addClass("future");    
     }
-
-    // taskArray.forEach(element => {
-    //     if (element.savedTime === blockHour) {
-    //         $(this).children(".description").val(element.text);
-    //     }
-        
-    // });
+    
+    taskArray.forEach(element => {
+        if (element.savedTime === blockHour) {
+            $(this).children(".description").val(element.text);
+        }
+    });
 });
 
 $(".saveBtn").click(function () {
     var text = $(this).siblings(".description").val();
-   
     var savedTime = $(this).siblings(".hour").text().trim();
     var TimeText = { text, savedTime };
     taskArray.push(TimeText);
         
-    localStorage.setItem("tasks",JSON.stringify(taskArray));
-})
+    localStorage.setItem("tasks", JSON.stringify(taskArray));
+});
 
 
 
